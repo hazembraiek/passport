a7ef05909a2c76707c90e8364f5fa3794937866b
Rayen <mansourirayen@takiacademyteam.com>
2022-09-02 11:44:25 +0100

    remove complex mock

--- tests/controllers/acessController/access.controller.test.js
+++ tests/controllers/acessController/access.controller.test.js
@@ -1,11 +1,36 @@
-
-const { existingUser } = require("./mock.js"); // 
+const mongoose = require("mongoose");
 const supertest = require("supertest");
+const { UserRepository } = require("../../../src/db/repository");
+const { MongoMemoryServer } =  require("mongodb-memory-server");
 const app = require("../../../src/app");
 
-//falgs for test
-let REFRESH_TOKEN; 
-let ACCESS_TOKEN;
+
+
+//mock send mail function
+jest.mock("../../../src/utils/mailSender.js",() => {
+    return jest.fn((mailOptions) => global.RESET_LINK = mailOptions.text.split("/").pop());
+});
+
+
+const user = {
+    email:"mansouri@rayen.com",
+    password:"pass1",
+    name:"doubleA"
+}
+
+beforeAll(async () => {
+    //if mongoose is connected close connection to remote DB
+    if(mongoose.connection.readyState === 0)
+        await mongoose.disconnect();
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+    await UserRepository.create(user);
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+})
 
 describe("Test Login",() => {
     const server = supertest(app);
@@ -17,63 +42,52 @@ describe("Test Login",() => {
         expect(response.body.message).toMatch("account does not exist");
     });
 
-    it("should send error when email is only sent",async () => {
-        const nonExistingUser = {
+    it("should send error when sending wrong email",async () => {
+        const nonuser = {
             email:"john@service.domain",
         }
         const response = await server
                                .post("/v1/login")
-                               .send(nonExistingUser);
-        expect(response.status).toBe(404);
-        expect(response.body).toHaveProperty("message");
-        expect(response.body.message).toMatch("account does not exist");
-    });
-    
-    it("should send error when password is only sent",async () => {
-        const nonExistingUser = {
-            password:"pass123"
-        }
-        const response = await server
-                               .post("/v1/login")
-                               .send(nonExistingUser);
+                               .send(nonuser);
         expect(response.status).toBe(404);
         expect(response.body).toHaveProperty("message");
         expect(response.body.message).toMatch("account does not exist");
     });
     it("should send error when password is incorrect ",async () => {
-        const user = {
-            ...existingUser,
+        const userWithWrongPass = {
+            ...user,
             password:""
         }
         const response = await server
                                .post("/v1/login")
-                               .send(user);
+                               .send(userWithWrongPass);
         expect(response.status).toBe(422);
         expect(response.body).toHaveProperty("message");
         expect(response.body.message).toMatch("invalid password or email");
     });
     
     it("should login",async () => {
+      
         const response = await server
                                .post("/v1/login")
-                               .send(existingUser);
+                               .send(user);
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty("access_token");
         expect(response.body).toHaveProperty("refresh_token");
-        REFRESH_TOKEN = response.body.refresh_token;
-        ACCESS_TOKEN = response.body.access_token;
+        global.REFRESH_TOKEN = response.body.refresh_token;
+        global.ACCESS_TOKEN = response.body.access_token;
     });
 })
 
 describe("test forgot password",() => {
     const server = supertest(app);
-    it("should send an error whent account",async () => {
-        const nonExistingUser = {
-            email:"john@service.domain",
+    it("should send an error when account does not exist",async () => {
+        const nonuser = {
+            email:"bla@bla.com",
         }
         const response = await server
                                .post("/v1/login/forgot_password")
-                               .send(nonExistingUser);
+                               .send(nonuser);
         expect(response.status).toBe(404);
         expect(response.body).toHaveProperty("message");
         expect(response.body.message).toBe("Account does not exist");
@@ -81,7 +95,7 @@ describe("test forgot password",() => {
     it("should send an reset link",async () => {
         const response = await server
                                .post("/v1/login/forgot_password")
-                               .send(existingUser);
+                               .send(user);
         
         expect(response.status).toBe(200);
         expect(response.body).toHaveProperty("message");
@@ -95,7 +109,7 @@ describe("test refresh token",() => {
     it("should send an access token",async () => {
        
         const token = {
-            token:REFRESH_TOKEN,
+            token:global.REFRESH_TOKEN,
         }
         const response = await server
                                .post("/v1/login/token")
@@ -117,7 +131,7 @@ describe("test refresh token",() => {
 
     it("should throw an error when send non existing token",async () => {
         const token = {
-            token:ACCESS_TOKEN,
+            token:global.ACCESS_TOKEN,
         }
         const response = await server
                                .post("/v1/login/token")
